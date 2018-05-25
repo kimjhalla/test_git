49ae5a01d8578473fcbc1b31b343d30d7a290ab9
jhkim <jhkim@DESKTOP-A0D3DHM>
2018-05-25 18:06:00 +0900

    - ajax를 활용한 로그인 처리 로직 추가(결과 에러나는 이유 찾아야됨)

--- WebProject/src/main/java/com/jhkim/project/controller/UserController.java
+++ WebProject/src/main/java/com/jhkim/project/controller/UserController.java
@@ -4,14 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jhkim.project.dto.UserDTO;
 import com.jhkim.project.mapper.UserMapper;
+import com.jhkim.project.result.BasicResult;
 
 @Controller
 public class UserController {
@@ -19,13 +24,29 @@ public class UserController {
 	UserMapper userMapper;
 	
 	@RequestMapping(value="/Login", method=RequestMethod.POST)
-	public String Login(String id,String pw,Model model){
-		Map<String,String> param = new HashMap<String,String>();
-		param.put("userId", "");
-		param.put("userPw", "");
-		UserDTO dto = userMapper.selectUserData(param);
+	@ResponseBody
+	public BasicResult Login(@RequestParam Map<String,String> param,HttpServletRequest request, Model model){
+		BasicResult result = new BasicResult();
+		try{
+			
 		
-		return "";
+		System.out.println(param.get("userId"));
+		System.out.println(param.get("userPw"));
+		
+		UserDTO dto = userMapper.selectUserData(param);
+		System.out.println("after");
+		if(dto == null){
+			System.out.println("null");
+			result.setResultCode("EF001");
+			result.setResultMsg("로그인 실패");
+		}
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("result");
+		return result;
 	}
 	
 	@RequestMapping(value="/Join", method=RequestMethod.POST)
--- WebProject/src/main/webapp/WEB-INF/views/home.jsp
+++ WebProject/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,16 +1,45 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
 	<script type="text/javascript" src="<c:url value="/resources/js/jquery-3.3.1.js/"/>"></script>
+	<link href="<c:url value="/resources/css/home.css"/>" rel="stylesheet" ></link>
+	<link href="<c:url value="/resources/css/home.css"/>" rel="stylesheet" ></link>
+	<script type="text/javascript">
+		function ajax(){
+			jQuery.ajax({
+				type:"POST",
+				url:"Login.do",
+				data:{
+					userId : $("#userId").val(),
+					userPw : $("#userPw").val()					
+				},
+				datatype:"json",
+				contenttype:"application/json;",
+				success:function(obj){console.log('!!');},
+				error : function(xhr,status,error){
+					console.log(xhr);
+				}
+			})
+		}
+	
+	</script>
 	<title>Home</title>
 </head>
 <body>
-<form method="post" action="Login.do">
-	<input type="text" id="userId" value="">
-	<input type="password" id="userPw" value="">	
-	<input type="submit" value="로그인">
-</form>
+<div id="div_root">
+<div id="div_top">
+<div id="div_login">
+	<input type="text" name="userId" id="userId" value=""><br/>
+	<input type="password" name="userPw" id="userPw" value=""><br/>	
+	<input type="button" id="joinBtn" value="가입"><input type="button" id="loginBtn" value="로그인" onclick="ajax();">
+<div id="div_main">
+	
+</div>
+</div>
+</div>
+</div>
 </body>
 </html>
